1711932cdd94eb2d4a3672441f4e34a90729cb6e
katarem <crismigesp@gmail.com>
2023-03-13 01:12:42 +0000

    mi primera app en js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  document.title = 'Croso.gg'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id='epico'>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,7 +11,47 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const usrInput = document.createElement('input')
+const api_key = '0d3751c7e7fb4680849513005bff7b6f'
+
+const button = document.createElement('button')
+button.innerText = 'click me'
+button.addEventListener('click', () => {
+  getWeather()
+})
+
+async function getWeather(){
+  const city_name = usrInput.value.toLowerCase()
+  if(usrInput.value==='')
+    window.alert('Añada una ciudad')
+  const response = await fetch(`https://api.weatherbit.io/v2.0/current?&city=${city_name}&country=ES&lang=es&key=${api_key}`)
+  const tiempo = await response.json()
+  const objeto = await tiempo.data[0]
+  const city = await objeto.city_name
+  const icon = await objeto.weather.icon
+  const temp = await objeto.temp
+  const wdata = await objeto.weather.description
+  const contenedor = document.createElement('div')
+  const cityView = document.createElement('p')
+  const iconView = document.createElement('img')
+  const weatherData = document.createElement('p')
+  iconView.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`
+  weatherData.textContent = wdata
+  cityView.textContent = city + ' ' + Math.round(temp) + 'ºC'
+  contenedor.id = 'omg'
+  contenedor.appendChild(iconView)
+  contenedor.appendChild(weatherData)
+  contenedor.appendChild(cityView)
+  const epico = document.getElementById('epico')
+  if(epico.lastChild)
+    epico.removeChild(epico.lastChild)
+  epico.appendChild(contenedor)
+}
+button.id = 'a'
+usrInput.id = 'inp'
+const divi = document.createElement('div')
+divi.append(usrInput,button)
+divi.id = 'divi'
+document.body.append(divi)
+
 reportWebVitals();
